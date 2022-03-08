6aa5599f527a6f5ca1283f2400c2c134ffdddaad
Shubham0850 <rajshubham0850@gmail.com>
2022-03-08 08:49:41 +0530

    auth is done

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,29 @@ import { Anchor, Box, Heading, Paragraph } from 'grommet'
 import React from 'react'
 
 import ConnectButton from './ConnectButton'
+import Welcome from './pages/Welcome'
 
 export default function App() {
   const [connection] = useConnection()
 
   return (
     <Box align="center" flex pad="large">
-      <Heading>Self.ID example app</Heading>
+      {/* <Heading>Self.ID example app</Heading>
       <Box pad="medium">
         <AvatarPlaceholder
           did={connection.status === 'connected' ? connection.selfID.id : 'self.id'}
           size={120}
         />
-      </Box>
-      <ConnectButton />
+      </Box> */}
+      {/* <ConnectButton />
       <Paragraph>
         Learn more about the{' '}
         <Anchor href="https://developers.ceramic.network/tools/self-id/overview/">
           Self.ID SDK
         </Anchor>
         .
-      </Paragraph>
+      </Paragraph> */}
+      <Welcome/>
     </Box>
   )
 }
